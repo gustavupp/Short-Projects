78e1357de28c99162f2b96ac0c7a11f75680c318
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-02 19:02:24 +1000

    project finalized
    project finalized

--- ChessBoard/ChessBoard.js
+++ ChessBoard/ChessBoard.js
@@ -1,34 +1,47 @@
-//const p = document.getElementById('test');
-//const row = document.querySelector('.row');
-//const column = document.querySelector('.column');
 const button = document.querySelector('#submit');
 
 button.addEventListener('click', chess);
 
 function chess() {
+    let resetTd = document.querySelectorAll('td');
+    let resetTr = document.querySelectorAll('tr');
+    let resetTbody = document.querySelectorAll('tBody');
+    //resetTd.remove();
+    //resetTr.remove();
+    //resetTbody.remove();
+
     let givenWidth = document.querySelector('#getWidth').value;
     let givenHeight = document.querySelector('#getHeight').value;
    
     var tblBody = document.createElement('tbody');
     var parentTable = document.getElementById('chessboard');
 
-    for(let i = 0; i < givenHeight; i++) {
-        for (j = 0; j < givenWidth; j++) {
+    // first look creates <tr> then after inner look grabs <tr> and appends it to <tbody>
+        for(let i = 0; i < givenHeight; i++) {
+        var row = document.createElement('tr');
+       
+    // second loop creates cells '<td>' then grabs and appends <td> to <tr>    
+         for (j = 0; j < givenWidth; j++) {
             if ((j % 2 === 0 && i % 2 === 0) || (j % 2 !== 0 && i % 2 !== 0)) {
                 var cell = document.createElement('td');
+                row.appendChild(cell);
+                cell.style.backgroundColor = 'black';
             } else {
                 var cell = document.createElement('td');
+                row.appendChild(cell);
+                cell.style.backgroundColor = 'beige';
             }
         }
-        var row = document.createElement('tr');
-        row.appendChild(cell);
         var parentRow = document.querySelector('tr');
         tblBody.appendChild(row);
     }
+    // the whole table gets appended to <tblbody>
     parentTable.appendChild(tblBody);  
 };
 
-/*  ----------Original Function--------------
+// post that helped me get the orignal function to handle the DOM - https://stackoverflow.com/questions/14643617/create-table-using-javascript
+
+/*  ----------Original Function to print a Chess Pattern--------------
 function chess(w, h) {
     let grid = [];
     for(let i = 0; i < h; i++) {
