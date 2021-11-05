1c1838dbd970dee254163cb0a4f89c0ea76ea04c
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-05 17:47:03 +1000

    fixed hidden menu when left reviewed
    fixed hidden menu when left reviewed

--- Nav-bar/navbar.js
+++ Nav-bar/navbar.js
@@ -1,8 +1,8 @@
 //grab DOM elements
 const hiddenMenu = document.querySelector('.links-mobile');
 const toggle1 = document.querySelector('.nav-toggle1');
-const toggle2 = document.querySelector('.nav-toggle2');
 
+// function to review or hide menu onclick
 function hideReviewMenu(targetMenu) {
     if (targetMenu.style.display === 'none') {
         targetMenu.style.display = 'block';
@@ -11,8 +11,22 @@ function hideReviewMenu(targetMenu) {
     }
 }
 
+function onResize() {
+    if (window.outerWidth > 600) {
+        hiddenMenu.style.display = 'none'
+    }
+}
+
+//hideReviewMenu() is called on click
 toggle1.addEventListener('click', () => hideReviewMenu(hiddenMenu));
+
+//hides menu on mouseleave
 hiddenMenu.addEventListener('mouseleave', (hiddenMenu) => hiddenMenu.target.style.display = 'none');
+
+//hides menu (if left open) when window is resized beyond 600px
+window.addEventListener('resize', onResize);
+
+
 
 //I could do it this way too
 
