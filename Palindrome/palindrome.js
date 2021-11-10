7bf4c57e92965e25c8add7706fa3fdccfc2ccf0a
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-10 15:30:02 +1000

    made a palindrome checker
    made a palindrome checker

---
+++ Palindrome/palindrome.js
@@ -0,0 +1,39 @@
+
+palindrome('   &han^n ah ');
+
+function palindrome(str) {
+    const string = str;
+
+    //clean string
+    const lowerString = string.toLowerCase();
+    const noAlphaCaracters = lowerString.replace(/[\W_]/g, ""); 
+    console.log(noAlphaCaracters);
+
+    const turnIntoArray = noAlphaCaracters.split(""); //turn into array
+
+    //this extra loop wasnt planned.. but the cleaning of the string left undefined data on the array therefore array.length wasn't working.. that is the fix for array.length...it counts only non-undefined values.
+    var len = 0;
+    for (let i = 0; i < turnIntoArray.length; i++) {
+    if (turnIntoArray[i] !== undefined) {
+        len++;
+    }
+    }
+    //console.log(len);
+
+    //create new array in reverse order
+    let reverseArr = [];
+    for (let i = 0; i < len; i++) {
+        reverseArr += turnIntoArray.pop();
+    }
+
+    console.log(reverseArr);
+
+    //check if initia string equals reversed string
+    if (noAlphaCaracters === reverseArr) {
+        console.log('Palindrome');
+        return true;
+    } else {
+        console.log('Not Palindrome');
+        return false;
+    }   
+} 
