c99fe6962cbe42b207a6283766d7a9302fa250ed
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-12 11:23:54 +1000

    caesars cipher challenge done
    managed to make the undefined value get replaced by a empty space with an extra for loop

--- Caesars Cipher/app.js
+++ Caesars Cipher/app.js
@@ -1,26 +1,37 @@
 function rot13(str) {
 
-    const arr = ["","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const arr = [" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-    let string = str;
-    let result = 0;
+    let result = 0; //initiate global variables
     let newResult = [];
 
-    for (let i = 0; i < string.length; i++) {
-        result += parseInt(arr.indexOf(string[i])) - 13;
-        if (result <= 0) {
-            result += 26;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/[A-Z|a-z]/ig)) { //check if value is a letter
+            result += parseInt(arr.indexOf(str[i])) - 13; //had to use parseInt to be able to subtract
+            if (result <= 0) { //if 0 or negative by adding 26(number of alphabet letters) you take the value back to the matching index
+                result += 26;
+            }
+            newResult[i] = result;  //assigns value of result to a different variable
+            result = 0; //clear var result after looping through again
+        }  else {
+            newResult[i] = str[i]; //if value at current index is not a letter, assign its own value to newResult
         }
-        newResult[i] = result;
-        result = 0;
+    }   
+
+    for (let i = 0; i < newResult.length; i++) { //loop thru again to assign the matching index numbers to the matching letters
+        if (!isNaN(newResult[i])) { //if a number (at first I tried using !newResult[i].match(/[0-9]/gi) but newResult is not a string.)
+            newResult[i] = arr[newResult[i]]; //e.g. if newResult[1] = arr[A]
+        }  
     }
 
-    for (let i = 0; i < newResult.length; i++) {
-        newResult[i] = arr[newResult[i]];
+    for (let i = 0; i < newResult.length; i++) { //had to add another loop to replace undefined with a " "
+        if (typeof newResult[i] == 'undefined') {  
+            newResult[i] = arr[0];
+        } 
     }
 
-    newResult = newResult.join("");
+    newResult = newResult.join(""); //lastly, join the array values.
     return newResult;
 }
 
-console.log(rot13("SERR PBQR PNZC"));
+console.log(rot13("SERR YBIR?"));
