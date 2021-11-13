23eb80c70b1c63c6df6f16e10d0d5b40dad96bda
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-13 12:55:09 +1000

    started working on the cash register challenge
    started working on the cash register challenge

--- Check Cash Register/app.js
+++ Check Cash Register/app.js
@@ -7,31 +7,51 @@ function checkCashRegister(price, cash, cid) {
             "OPEN"
         ],
         change: [
-            [],
+            [0],
             ['cid'],
             ['cid']
         ]
     }
 
-    const CURRENCY_UNIT = [['PENNY', 0.01]];
+    const CURRENCY_UNIT = [["ONE HUNDRED", 100],["TWENTY", 20],["TEN", 10],["FIVE", 5],["ONE", 1],["QUARTER", 0.25],["DIME", 0.1],["NICKEL", 0.05],["PENNY", 0.01]];
 
-    console.log(CURRENCY_UNIT[0][1]);
-
-
-    let change;
+    let finalChange = [];  
     let changeDue = cash - price;
+    changeDue = changeDue.toFixed(2);
     let cidTotal = 0;
 
     for (let value in cid) {
        cidTotal += cid[value][1];
     }
-    console.log(cidTotal);
+    cidTotal = cidTotal.toFixed(2);
 
-    if (changeDue > cidTotal) return `{status: ${myObj.status[0]}, change: []}`;
+    if (changeDue > cidTotal) return `{status: ${myObj.status[0]}, change: ${myObj.change[0]}}`;
     if (changeDue === cidTotal) return `{status: ${myObj.status[1]}, change: ${cid}}`;
-    if (changeDue < cidTotal)
+    if (changeDue < cidTotal) {
 
-    return change;
+            let changeSum = changeDue;
+
+
+            for (let i = 0; i < CURRENCY_UNIT.length; i++) {
+                if (CURRENCY_UNIT[i][1] < changeSum){
+                    changeSum -= CURRENCY_UNIT[i][1];
+                    finalChange.push(CURRENCY_UNIT[i]);
+                    i--;
+                    console.log(changeSum);
+                        if (CURRENCY_UNIT[i][1] === changeSum) {
+                            changeSum -= CURRENCY_UNIT[i][1];
+                            finalChange.push(CURRENCY_UNIT[i]);
+                            changeSum = changeDue - finalChange ///keep working from here
+                        }
+                } else if (CURRENCY_UNIT[i][1] === changeSum) {
+                    return `{status: ${myObj.status[2]}, change: ${CURRENCY_UNIT[i][1]}}`;
+                }
+                console.log(changeSum);
+            }
+            console.log(changeSum);
+            console.log(finalChange);
+        }    
+    //return finalChange;
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.22, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
