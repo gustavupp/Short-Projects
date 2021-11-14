e3b1cd2009b5c8c4d6963017c213954b9c1fe47f
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-14 19:05:28 +1000

    got it to work but not quite the way I want
    it works but not exactly how the challenge wants..i'll get there

--- Check Cash Register/app.js
+++ Check Cash Register/app.js
@@ -1,63 +1,57 @@
 function checkCashRegister(price, cash, cid) {
-
-    const myObj = {
-        status: [
-            "INSUFFICIENT_FUNDS",
-            "CLOSED",
-            "OPEN"
-        ],
-        change: [
-            [0],
-            ['cid'],
-            ['cid']
-        ]
-    }
-
-    const CURRENCY_UNIT = [["ONE HUNDRED", 100],["TWENTY", 20],["TEN", 10],["FIVE", 5],["ONE", 1],["QUARTER", 0.25],["DIME", 0.1],["NICKEL", 0.05],["PENNY", 0.01]];
-
-    let finalChange = [];  
+ 
     let changeDue = cash - price;
     changeDue = changeDue.toFixed(2);
     let cidTotal = 0;
+
+    const myObj = [
+        {status: "INSUFFICIENT_FUNDS", change: []},
+        {status: "CLOSED", change: [cid]},
+        {status: "OPEN", change: []}
+    ]
+    
+    console.log(myObj[2].change);
+
+    const CURRENCY_UNIT = [["ONE HUNDRED", 100],["TWENTY", 20],["TEN", 10],["FIVE", 5],["ONE", 1],["QUARTER", 0.25],["DIME", 0.1],["NICKEL", 0.05],["PENNY", 0.01]];
 
     for (let value in cid) {
        cidTotal += cid[value][1];
     }
     cidTotal = cidTotal.toFixed(2);
 
-    if (changeDue > cidTotal) return `{status: ${myObj.status[0]}, change: ${myObj.change[0]}}`;
-    if (changeDue === cidTotal) return `{status: ${myObj.status[1]}, change: ${cid}}`;
+    if (changeDue > cidTotal) return myObj[0];
+    if (changeDue === cidTotal) return myObj[1];
     if (changeDue < cidTotal) {
 
-            let changeSum = changeDue;
+        let changeSum = changeDue;
 
-            for (let i = 0; i < CURRENCY_UNIT.length; i++) {
-                if (CURRENCY_UNIT[i][1] < changeSum){
-                    changeSum -= CURRENCY_UNIT[i][1];
-                    finalChange.push(CURRENCY_UNIT[i]);
+        for (let i = 0; i < CURRENCY_UNIT.length; i++) {
+            if (CURRENCY_UNIT[i][1] < changeSum){
+                changeSum -= CURRENCY_UNIT[i][1];
+                myObj[2].change.push(CURRENCY_UNIT[i]);
+                
+                    if (CURRENCY_UNIT[i][1] === changeSum) {
+                        changeSum -= CURRENCY_UNIT[i][1];
+                        myObj[2].change.push(CURRENCY_UNIT[i]);
+                        return myObj[2];
+                    }
                     i--;
-                        if (CURRENCY_UNIT[i][1] === changeSum) {
-                            changeSum -= CURRENCY_UNIT[i][1];
-                            finalChange.push(CURRENCY_UNIT[i]);
-                            changeSum = changeDue - finalChange ///keep working from here
-                        }
-                } else if (CURRENCY_UNIT[i][1] === changeSum) {
-                    return `{status: ${myObj.status[2]}, change: ${CURRENCY_UNIT[i][1]}}`;
-                }
-            }
 
-           console.log(finalChange);
-           let newFinalChange = [];
+            } else if (CURRENCY_UNIT[i][1] === changeSum) {
+                return myObj[2];
+            }
+        }
+
+           /*let newFinalChange = [];
            newFinalChange[0] = finalChange[0];
            console.log(newFinalChange);
            for (let i = 1; i < finalChange.length; i++) {
-                if (finalChange[i][0] == newFinalChange[i-1][0]) {
-                    newFinalChange[i-1][1] * 2;
-                    console.log(newFinalChange[i-1]);
+                if (finalChange[i][0] === newFinalChange[i-1][0]) {
+                    newFinalChange[i-1][1] = newFinalChange[i-1][1] * 2;
                 }
-           }
-    }    
-    //return finalChange;
+            }*/  
+    }
+    return myObj[2];
 }
 
-console.log(checkCashRegister(19.22, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
