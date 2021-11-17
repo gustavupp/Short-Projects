530820bea2f90e7017567ef741c084dbfd982b92
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-17 23:04:37 +1000

    added backtoTop button
    added back to top button

--- Scroll Project/app.js
+++ Scroll Project/app.js
@@ -1,10 +1,21 @@
 // dynamically display date
 const date = document.getElementById("date").innerHTML = new Date().getFullYear();
 
+//open menu
 const toggleBtn = document.querySelector(".nav-toggle");
 const navDiv = document.querySelector(".links-container");
-
+const navLinksList = document.querySelector(".links-list");
 
 toggleBtn.addEventListener("click", function() {
-    navDiv.classList.toggle("show-nav");
+    //navDiv.classList.toggle("show-nav"); one way of doing it..not too dynamic if container height changes
+
+   const navDivHeight = navDiv.getBoundingClientRect().height;
+   const navLinksListHeight = navLinksList.getBoundingClientRect().height;
+    console.log(navDivHeight);
+    console.log(navLinksListHeight);
+   if (navDivHeight === 0) {
+    navDiv.style.height = `${navLinksListHeight}px`;
+   } else {
+       navDiv.style.height = 0;
+   } 
 });
