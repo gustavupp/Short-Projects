b126336e06d0d4cbad9c1061e106ac76f82f2cc2
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-18 21:49:40 +1000

    I have a working website at this stage..
    there are still a few things to do, but overall all features work

--- Scroll Project/app.js
+++ Scroll Project/app.js
@@ -12,18 +12,40 @@ toggleBtn.addEventListener("click", function() {
    const navDivHeight = navDiv.getBoundingClientRect().height;
    const navLinksListHeight = navLinksList.getBoundingClientRect().height;
 
-   if (navDivHeight === 0) {
-    navDiv.style.height = `${navLinksListHeight}px`;
-   } else {
-       navDiv.style.height = 0;
-   } 
+   if (navDivHeight === 0) navDiv.style.height = `${navLinksListHeight}px`;
+   else navDiv.style.height = 0;
 });
 
-//fixed navbar when scrolled past x pixels
+//navbar becomes fixed when scrolled past x pixels
 const navCenter = document.querySelector(".nav-center");
 const navCenterHeight = navCenter.getBoundingClientRect().height;
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > navCenterHeight) navCenter.classList.add("fixed-navbar");
     else navCenter.classList.remove("fixed-navbar");
+});
+
+//scroll to top button appears after scrolled x pixels
+const scrollTopBtn = document.querySelector(".scroll-link-btn");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 400) scrollTopBtn.classList.add("show-scroll-btn");
+    else scrollTopBtn.classList.remove("show-scroll-btn");
+});
+
+//scrolls to the right height when nav buttons are clicked
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener("click", function(e) {
+        //prevent Default
+        e.preventDefault();
+
+        //navigates to a specific spot on the page
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        let position = element.offsetTop;
+        window.scrollTo({left: 0, top: position - 59});
+        navDiv.style.height = 0;
+    });
 });
