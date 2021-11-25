f75e30a0127afbf1b928142388d751836080ef5b
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-25 21:10:07 +1000

    todolist app working
    todo list app finilized

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -10,6 +10,7 @@ const CLEAR_BTN = document.querySelector(".clear-btn");
 //*********EVENT LISTENERS********
 TODO_FORM.addEventListener("submit", addTodoItem);
 CLEAR_BTN.addEventListener("click", clearAll);
+window.addEventListener("DOMContentLoaded", displayCurrentList);
 
 //DEFAULT SETTINGS
 let editMode = false;
@@ -63,8 +64,8 @@ function addTodoItem(e){
         setBackToDefault(); //setback to default
 
     } else if (INPUT_VALUE && editMode) {
-        editElement.innerHTML = INPUT_TEXT.value; //assign input new value to the element you are editing
-        editLocalStorage(editID); //update local storage
+        editElement.innerHTML = INPUT_VALUE; //assign input new value to the element you are editing
+        editLocalStorage(editID, INPUT_VALUE); //update local storage
         SUBMIT_BTN.innerHTML = "Submit"; //change submit button innerHTML back to Submit
         displayAlert(ALERT, "Item Updated", "green");
         setBackToDefault(); //setback to default
@@ -80,7 +81,7 @@ function displayAlert(node, text, alertColor){
     node.classList.add(`alert-${alertColor}`);
 
     setTimeout(() => {
-        node.textContent = "";
+        node.textContent = "invisible alert";
         node.classList.remove(`alert-${alertColor}`);
     }, 1000);
 }
@@ -104,7 +105,7 @@ function clearAll(){
     }
     CLEAR_BTN.classList.remove("show-container");
     displayAlert(ALERT, "List Cleared!", "red");
-    //localStorage.removeItem("ALL_ITEMS");
+    localStorage.removeItem("localStorageList");
     setBackToDefault();
 }
 
@@ -134,19 +135,100 @@ function editTodoItem(e){
     editElement = EDIT_ELEMENT;
 }
 
+//display current list
+function displayCurrentList(){
+    let localStorageList = getLocalStorage();
+    localStorageList = localStorageList.forEach(function(item){
+
+        //here you go through the same process you went through to first add the item to the DOM
+        const NEW_INDIVIDUAL_ITEM = document.createElement("article"); //create an article
+        NEW_INDIVIDUAL_ITEM.classList.add("individual-item"); //added the CSS styling to the new item
+        const ATTRIBUTE = document.createAttribute("data-id"); //create a dataset atribute
+        ATTRIBUTE.value = item.id; //assign the item.id to the attribute
+        NEW_INDIVIDUAL_ITEM.setAttributeNode(ATTRIBUTE); //set ATTRIBUTE to NEW_INDIVIDUAL_ITEM
+
+        //create child elements and append to NEW_INDIVIDUAL ITEM
+        NEW_INDIVIDUAL_ITEM.innerHTML = 
+        `<div class="title">${item.value}</div>
+        <div class="btn-container">
+            <button type="button" class="edit-btn icon">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button type="button" class="delete-btn icon">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>`;
+
+        //THIS IS FOR THE DELETE AND EDIT BUTTON
+        //select and add event listeners to buttons after they have been added to the DOM
+        const DELETE_BTN = NEW_INDIVIDUAL_ITEM.querySelector(".delete-btn");
+        const EDIT_BTN = NEW_INDIVIDUAL_ITEM.querySelector(".edit-btn");
+        DELETE_BTN.addEventListener("click", deleTodoItem);
+        EDIT_BTN.addEventListener("click", editTodoItem);
+
+        //apend item to its parent
+        TODO_ITEMS.appendChild(NEW_INDIVIDUAL_ITEM);
+        NEW_INDIVIDUAL_ITEM.classList.add("show-container");
+        CLEAR_BTN.classList.add("show-container");
+        return NEW_INDIVIDUAL_ITEM; 
+    });
+
+
+    
+}
+
 //*********LOCAL STORAGE********
 //add to local storage
 function addToLocalStorage(id, value){
-    console.log("item added to local storage");
+    const todoItem = {id, value}; // grab input value and id and assign to another variable as an object.
+    
+    //call the getLocalStorage function and assign the return to the variable localStorageList
+    let localStorageList = getLocalStorage();
+    localStorageList.push(todoItem); //push the item into the array
+    localStorage.setItem("localStorageList", JSON.stringify(localStorageList)); //set the whole array back to the local storage.
 };
 
 //remove from local storage
-function removeFromLocalStorage(id){
-    console.log("removed from local storage");
+function removeFromLocalStorage(ID){
+    let localStorageList = getLocalStorage();
+
+    localStorageList = localStorageList.filter(function(item){ //modifies the original array excluding the matching item.id
+        if (item.id !== ID){
+            return item;
+        }
+    });
+    localStorage.setItem("localStorageList", JSON.stringify(localStorageList)); //set new array to the local storage
 }
 
 //update local storage
-function editLocalStorage(id){
-    console.log("local storage updated");
+function editLocalStorage(ID, VALUE){
+    let localStorageList = getLocalStorage();
+    localStorageList = localStorageList.map(function(item){
+        if (item.id === ID){
+            item.value = VALUE;
+        }
+        return item;
+    });
+    localStorage.setItem("localStorageList", JSON.stringify(localStorageList));
 }
+
+function getLocalStorage(){
+    //check if there are item in the local storage; if there are, fetch them; if there aren't, return an empty array.
+    return localStorage.getItem("localStorageList")? JSON.parse(localStorage.getItem("localStorageList")) : [];
+}
+
+//local storage API
+//setItem
+//getItem
+//removeItem
+//save as strings
+
+/*localStorage.setItem("testKey",JSON.stringify(["item1", "item2"]));
+
+let vari = JSON.parse(localStorage.getItem("testKey"));
+
+localStorage.removeItem("testKey");
+
+console.log(vari);*/
+
 //*********SETUP ITEMS***********
