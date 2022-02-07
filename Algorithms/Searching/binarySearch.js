40ed4a0c60a3327af49f753fb0e50bc37cd97329
Gustavo Pereira <xgustavux@hotmail.com>
2022-02-07 22:33:30 +1000

    binary search implemented

--- Algorithms/Searching/binarySearch.js
+++ Algorithms/Searching/binarySearch.js
@@ -1,19 +1,31 @@
+//iterative approach
+function IterativeBinarySearch(searchList, value) {
+  let left = 0
+  let right = searchList.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+
+    if (value === searchList[middle]) return searchList[middle]
+    else if (value > searchList[middle]) {
+      left = middle + 1
+    } else {
+      right = middle - 1
+    }
+  }
+  return 'Value Not Found'
+}
+console.log(IterativeBinarySearch([1, 2, 3, 4, 5, 6, 7], 1))
+
+//recursive approach
 function binarySearch(searchList, value) {
-  if (searchList.length === 1 && searchList[0] === value) return value
-  if (searchList.length === 1) return 'Not Found'
+  if (searchList.length < 1) return 'Value Not Found'
 
-  let middleIndex = Math.floor(searchList.length / 2)
-  let middleValue = searchList[middleIndex]
-
-  if (value === middleValue) return middleValue
-
-  let left = searchList.slice(0, middleIndex)
-  let right = searchList.slice(middleIndex)
-
-  console.log({ middleValue })
-
-  if (value > middleValue) return binarySearch(right, value)
-  else return binarySearch(left, value)
+  let middle = Math.floor((0 + searchList.length - 1) / 2)
+  if (searchList[middle] === value) return searchList[middle]
+  else if (searchList[middle] > value)
+    return binarySearch(searchList.slice(0, middle), value)
+  else return binarySearch(searchList.slice(middle + 1), value)
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 5))
+//console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 5))
