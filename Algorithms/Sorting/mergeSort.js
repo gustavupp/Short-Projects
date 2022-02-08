e69f330addd0758a0aab4ebefc28a57fb4d408ef
Gustavo Pereira <xgustavux@hotmail.com>
2022-02-08 22:59:54 +1000

    merge sort done again

--- Algorithms/Sorting/mergeSort.js
+++ Algorithms/Sorting/mergeSort.js
@@ -1,4 +1,28 @@
-//devides array in 2 recursevely and calls merge on subarrays
+// //devides array in 2 recursevely and calls merge on subarrays
+// function mergeSort(arr) {
+//   if (arr.length <= 1) return arr
+
+//   let middle = Math.floor(arr.length / 2)
+//   let left = arr.slice(0, middle)
+//   let right = arr.slice(middle)
+
+//   return merge(mergeSort(left), mergeSort(right))
+// }
+
+// //sorts and merges subarrays
+// function merge(leftArr, rightArr) {
+//   let sorted = []
+
+//   while (leftArr.length && rightArr.length) {
+//     if (leftArr[0] < rightArr[0]) sorted.push(leftArr.shift())
+//     else sorted.push(rightArr.shift())
+//   }
+
+//   //return sorted.concat(rightArr).concat(leftArr) //ES5 Version
+//   return [...sorted, ...rightArr, ...leftArr]
+// }
+
+// [merge] [sort]
 function mergeSort(arr) {
   if (arr.length <= 1) return arr
 
@@ -9,17 +33,15 @@ function mergeSort(arr) {
   return merge(mergeSort(left), mergeSort(right))
 }
 
-//sorts and merges subarrays
-function merge(leftArr, rightArr) {
-  let sorted = []
+function merge(left, right) {
+  let result = []
 
-  while (leftArr.length && rightArr.length) {
-    if (leftArr[0] < rightArr[0]) sorted.push(leftArr.shift())
-    else sorted.push(rightArr.shift())
+  while (left.length && right.length) {
+    if (left[0] > right[0]) result.push(right.shift())
+    else result.push(left.shift())
   }
 
-  //return sorted.concat(rightArr).concat(leftArr) //ES5 Version
-  return [...sorted, ...rightArr, ...leftArr]
+  return [...result, ...left, ...right]
 }
 
 console.log(mergeSort([2, 4, 1, 0, -6, 98, 3, 6, 5, 7, 999, -299, 0, 1]))
--- Algorithms/Sorting/quickSort.js
+++ Algorithms/Sorting/quickSort.js
@@ -1,44 +1,23 @@
-// function quickSort(array) {
-//   if (array.length <= 1) return array
-//   else {
-//     let arr = array.slice()
-//     let newArray = []
-//     let key = [arr[0]]
-//     let right = []
-//     let left = []
-
-//     for (let i = 1; i < arr.length; i++) {
-//       if (arr[i] > key) right.push(arr[i])
-//       else left.push(arr[i])
-//     }
-
-//     return newArray.concat(quickSort(left), key, quickSort(right))
-//   }
-// }
-
-// console.log(
-//   quickSort([
-//     5, 43, 344, 3, 2, 5, 435, 2, 2, 1.2, 56.5, 3, 0, 435, 4, -6, -5, -65, -5,
-//   ])
-// )
-
 //[quick],to,[sort]
-function quickSort(arr) {
-  if (arr.length <= 1) return arr
+function quickSort(array) {
+  if (array.length <= 1) return array
+  else {
+    let newArray = []
+    let key = [arr[0]]
+    let right = []
+    let left = []
 
-  let result = []
-  let left = []
-  let right = []
-  let key = [arr[0]] //any value from array
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > key) right.push(arr[i])
+      else left.push(arr[i])
+    }
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > key) right.push(arr[i])
-    else left.push(arr[i])
+    return newArray.concat(quickSort(left), key, quickSort(right))
   }
-
-  return result.concat(quickSort(left), key, quickSort(right))
 }
 
 console.log(
-  quickSort([5, 43, 344, 2, 2, 1.2, 56.5, 56.6, 3, 0, 4, -6, -5, -65])
+  quickSort([
+    5, 43, 344, 3, 2, 5, 435, 2, 2, 1.2, 56.5, 3, 0, 435, 4, -6, -5, -65, -5,
+  ])
 )
