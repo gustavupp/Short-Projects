ca0f4e6e65fc50ae50e99f1889a59c99a78301f9
Gustavo Pereira <xgustavux@hotmail.com>
2022-02-09 22:55:04 +1000

    methods to add and remove nodes implemented

--- AlgorithmsAndDataStructures/DataStructures/linkedList.js
+++ AlgorithmsAndDataStructures/DataStructures/linkedList.js
@@ -1,17 +1,17 @@
 //linked list implementation
 function LinkedList() {
-  let listLength = 0
+  let length = 0
   let head = null
 
   //function that creates Nodes
-  const Node = function (element) {
-    this.element = element
+  const Node = function (value) {
+    this.value = value
     this.next = null
   }
 
   //get the size of linked list
   this.size = () => {
-    return listLength
+    return length
   }
 
   //get the head of linked list
@@ -20,27 +20,54 @@ function LinkedList() {
   }
 
   //add a new node to the linked list (end of list)
-  this.addToEnd = () => {
-    let newNode = new Node(element) //create a new node
+  this.addToEnd = (value) => {
+    let newNode = new Node(value) //create a new node
     if (head === null) head = newNode
     //if list is empty, set node as head
     else {
       let currentNode = head // mark head as current node
-
       while (currentNode.next) {
         //while there are elemements in the list keep moving forward
-        currentNode = currentNode.next //at every iteration, set current node as the next element
+        currentNode = currentNode.next //at every iteration, set current node as the next value
       }
-
       currentNode.next = newNode // when currentNode.next === null we leave the loop(reaching the end the linked list) and set currentNode.next to the node we created.
     }
-    listLength++ // increase the the lenght of the list
+    length++ // increase the the length of the list
   }
 
   //add a new node to linked list (beggining)
-  this.addToBeginning = () => {
-    let newNode = new Node(element) //create new Node
+  this.addToBeginning = (value) => {
+    let newNode = new Node(value) //create new Node
     newNode.next = head //point new node to head
     head = newNode //assign head to new node
+    length++ //add to length
+  }
+
+  //removes a node
+  this.remove = (value) => {
+    let currentNode = head //set current node as first node
+    let previousNode
+    //if value of current element (which is head at first) equals the value we want to delete, set head to next node
+    if (currentNode.value === value) head = currentNode.next
+    else {
+      while (currentNode.value !== value) {
+        //move through the list while value we havent found the value we want to delete
+        previousNode = currentNode //update previous node
+        currentNode = currentNode.next //update next node
+      }
+
+      previousNode.next = currentNode.next //once the node we want to delete is found, point the preious node.next to current.next, bypassing current node.
+    }
+    length-- //decrease length
   }
 }
+
+let myLinkedList = new LinkedList()
+myLinkedList.addToBeginning(11)
+myLinkedList.addToBeginning(10)
+myLinkedList.addToEnd(12)
+myLinkedList.addToEnd(13)
+myLinkedList.addToBeginning(9)
+myLinkedList.remove(10)
+
+console.log(myLinkedList.size(), myLinkedList.head())
