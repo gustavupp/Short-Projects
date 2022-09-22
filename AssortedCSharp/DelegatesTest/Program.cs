803386f9a45c70c32bdecd59f4e8fd505557ff08
Gustavo Pereira <xgustavux@hotmail.com>
2022-09-22 19:02:58 +1000

    recreating forEach and Select methods

---
+++ AssortedCSharp/DelegatesTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace DelegatesTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new List<char>() { 'a', 'b', 'c', 'd' };
+            myList.MyForEach((x,index) => Console.WriteLine(x));
+
+            var myCustomList = myList.MySelect((x, index) => {
+                x = 'G';           
+                Console.WriteLine(x);
+                return x;
+            });
+        }
+    }
+
+    public static class IterableExtension
+    {
+        public static void MyForEach<T>(this IEnumerable<T> collection, Action<T, int?> callback)
+        {
+            foreach (var item in collection.Select((value, i) => new { value, i }))
+            {
+                callback(item.value, item.i);
+            }
+
+            /*for (var i = 0; i < collection.Count(); i++)
+            {
+                callback(collection[i], i);
+            }*/
+        }
+
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> collection, Func<TSource, int?, TResult> callback)
+        {
+            
+            var returnCollection = new Collection<TResult>();
+
+            foreach (var item in collection.Select((value, i) => new { value, i }))
+            {
+                returnCollection.Add(callback(item.value, item.i));
+            }
+
+            return returnCollection;
+        }
+    }
+}
