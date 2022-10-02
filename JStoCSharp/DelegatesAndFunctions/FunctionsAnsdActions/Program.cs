17e141b042cbef2612b39dcf75e138223f0bf929
Gustavo Pereira <xgustavux@hotmail.com>
2022-10-02 12:41:28 +1000

    actions and functions js to c#

---
+++ JStoCSharp/DelegatesAndFunctions/FunctionsAnsdActions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FunctionsAnsdActions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> callback1 = delegate (string msg) {
+                Console.WriteLine(msg);
+            };
+
+            Action<string> callback = (msg) => Console.WriteLine(msg);
+
+            Helper.LetMeKnowWHenDonw("I am done.", callback);
+        }
+    }
+
+    public static class Helper
+    {
+        public static void LetMeKnowWHenDonw(string msg, Action<string> callback)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            callback(msg);
+        }
+    }
+}
