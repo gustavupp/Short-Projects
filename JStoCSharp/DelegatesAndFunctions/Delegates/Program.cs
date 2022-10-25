dab95b6aee8a65a12afe87010999a1ab532258a5
Gustavo Pereira <xgustavux@hotmail.com>
2022-10-26 06:41:36 +1000

    added more delegate examples

--- JStoCSharp/DelegatesAndFunctions/Delegates/Program.cs
+++ JStoCSharp/DelegatesAndFunctions/Delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Delegates
 {
@@ -9,13 +10,23 @@ namespace Delegates
         
         static void Main(string[] args)
         {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Predicate<int> predicate = x => x >= 5;
+            var allMatches = Array.FindAll(arr, delegate (int x)
+            {
+                return x >= 5;
+            });
+
+            var list = new List<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            list.FindAll(predicate);
+
             // can use custom declaration
-            MyLog log1 = delegate (string msg)
+            MyLog myFunction = delegate (string msg)
             {
                 Console.WriteLine(msg);
             };
 
-            MyLog log = msg => Console.WriteLine(msg);
+            MyLog myFunction1 = msg => Console.WriteLine(msg);
 
 
             // or generic
@@ -28,26 +39,57 @@ namespace Delegates
 
 
             //delegates as method parameters
-            Action<string> callback1 = delegate (string msg) 
+            //regular
+            Action<string> myFunction2 = delegate (string msg) 
             {
                 Console.WriteLine(msg); 
             };
 
-            Action<string> callback = msg => Console.WriteLine(msg);
+            //lambda
+            Action<string> myFunction3 = msg => Console.WriteLine(msg);
 
-            test.LetMeKnowWhenDone("I'm done.", callback);
-            test.LetMeKnowWhenDone("I'm done.", callback1);
-            /*test.LetMeKnowWhenDone("I'm done.", delegate (string msg) 
+            var testClass = new TestClass();
+
+            testClass.LetMeKnowWhenDone("I'm done.", myFunction3);
+            testClass.LetMeKnowWhenDone("I'm done.", myFunction2);
+
+            //regular function
+            testClass.LetMeKnowWhenDone("I'm done.", delegate (string msg)
             {
                 Console.WriteLine(msg);
-            };*/
+            });
 
-            test.LetMeKnowWhenDone("I'm done.", msg => Console.WriteLine(msg));
+            //in-line lambda
+            testClass.LetMeKnowWhenDone("I'm done.", msg => Console.WriteLine(msg));
+
+            TestClass.MyLoggerFunction myLoggerFunction = AnotherTest.FuncTest;
+            testClass.LetMeKnowWhenDone2("hiiii", myLoggerFunction);
         }
 
-        public static class test
+        public class TestClass
         {
-            public static void LetMeKnowWhenDone(string msg, Action<string> callback)
+            public delegate void MyLoggerFunction(string msg);
+
+            public Action<string> MyGenericLogger { get; set; }
+
+            public void LetMeKnowWhenDone(string msg, Action<string> callbackFunction)
+            {
+                callbackFunction(msg);
+            }
+
+            public void LetMeKnowWhenDone1(string msg)
+            {
+                MyGenericLogger(msg);
+            }
+            public void LetMeKnowWhenDone2(string msg, MyLoggerFunction myLoggerFunction)
+            {
+                myLoggerFunction(msg);
+            }
+        }
+
+        public class AnotherTest
+        {
+            public static void FuncTest(string msg)
             {
                 Console.WriteLine(msg);
             }
---
+++ OrganiseDirectory/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OrganiseDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Type the path to the desired folder, or leave press ENTER to set the path to the Downloads folder:\n");
+            var path = Console.ReadLine();
+
+            while (!Directory.Exists(path) && !string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("Invalid path. Type a valid path:\n");
+                path = Console.ReadLine();
+            }
+
+            if (string.IsNullOrWhiteSpace(path))
+                path = @$"C:\Users\{Environment.UserName}\Downloads";
+
+            CreateFolderAndMoveFiles(path);
+            DeleteEmptyFolders(path);
+
+            Console.ReadLine();
+        }
+
+        private static void DeleteEmptyFolders(string path)
+        {
+            var folders = Directory.GetDirectories(path);
+            foreach (var folder in folders)
+            {
+                Console.WriteLine(folder);
+                var directoryInfo = new DirectoryInfo(folder);
+
+                var subfolders = directoryInfo.EnumerateDirectories().ToList();
+                var filesInFolder = directoryInfo.EnumerateFiles().ToList();
+
+                if (!filesInFolder.Any() && !subfolders.Any())
+                {
+                    Console.WriteLine("No files/folders in this directory. Deleting folder...");
+                    try
+                    {
+                        Directory.Delete(folder);
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private static void CreateFolderAndMoveFiles(string path)
+        {
+            var files = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in files)
+            {
+                var ext = Path.GetExtension(file);
+                var fileFolder = $@"{path}\{ext}";
+                var fileName = Path.GetFileName(file);
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+
+                if (Directory.Exists(fileFolder))
+                {
+                    if (File.Exists($@"{path}\{ext}\{fileName}"))
+                        fileName = $"{fileNameWithoutExtension}-{DateTime.Now.Ticks}{ext}";
+
+                    File.Move(file, $@"{path}\{ext}\{fileName}", false);
+                }
+                else
+                {
+                    Directory.CreateDirectory(fileFolder);
+                    File.Move(file, $@"{path}\{ext}\{fileName}");
+                }
+            }
+        }
+    }
+}
