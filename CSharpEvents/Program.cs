d4a9cf21908c66837707ea517474fd89d5e60bc7
Gustavo Pereira <xgustavux@hotmail.com>
2022-11-28 08:21:33 +1000

    c# events

---
+++ CSharpEvents/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSharpEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create user obj
+            var newUser = new User("Gus", "gus@email.com");
+
+            //register methods you want to call once event is fired
+            var handler = new User.UserStatusHandler(SendWelcomeEmail);
+            newUser.AddMethodToCall(new User.UserStatusHandler(SendWelcomeInAppNotification));
+            newUser.AddMethodToCall(handler);
+
+            //the action that will trigger the event(execution of the methods)
+            newUser.AddUserToDb(newUser);
+            newUser.RemoveUserFromDb(newUser);
+        }
+
+        private static void SendWelcomeEmail(string msg)
+        {
+            Console.WriteLine("\n ********** Email ************");
+            Console.WriteLine(msg);
+            //SendEmail(msg)    
+        }
+
+        private static void SendWelcomeInAppNotification(string msg)
+        {
+            Console.WriteLine("\n e3 ********** InApp Notification ************");
+            Console.WriteLine(msg);
+            //SendNotification(msg)    
+        }
+    }
+}
+
---
+++ CSharpEvents/User.cs
@@ -0,0 +1,39 @@
+﻿public class User
+{
+    private string Name { get; set; }
+    private string Email { get; set; }
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public delegate void UserStatusHandler(string msg);
+    private UserStatusHandler listOfMethodsToCall;
+
+    public void AddMethodToCall(UserStatusHandler method)
+    {
+        listOfMethodsToCall += method;
+    }
+
+    public void RemoveMethodToCall(UserStatusHandler method)
+    {
+        listOfMethodsToCall -= method;
+    }
+
+    public void AddUserToDb(User user)
+    {
+        //Adds user to DB and then calls the methods that registered to be invoked once user has been added.
+
+        if (listOfMethodsToCall != null)
+            listOfMethodsToCall($"\n ************* User has just been added to DB {user.Name} | {user.Email} **************");
+    }
+     
+    public void RemoveUserFromDb(User user)
+    {
+        //Removes user from DB and then calls the methods that registered to be invoked once user has been removed.
+
+        listOfMethodsToCall?.Invoke($"\n ************* User has just been removed from DB {user.Name} | {user.Email} **************");
+    }
+}
+
